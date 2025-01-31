5988f56c8de7b2699b6367f33a2955413d157fed
GBR1405 <124008178+GBR1405@users.noreply.github.com>
2025-01-30 18:38:40 -0600

    Login and home

---
+++ TechSolutionsCenter/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechSolutionsCenter.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: LoginController/Details/5
+        public IActionResult Register(int id)
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ TechSolutionsCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechSolutionsCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
